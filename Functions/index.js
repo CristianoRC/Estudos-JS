d76fa576e2676f01afce3dee149c89c969bcc39c
Cristiano <contato@cristianoprogramador.com>
2020-12-23 13:16:09 -0300

    Exemplo de criaçao de funçao por construtor

---
+++ Functions/index.js
@@ -0,0 +1,9 @@
+/*
+    Construtor de função, aqui da para "ver" a função como um tipo no JS
+*/
+
+
+
+const olaMundo = new Function("nome", 'console.log(`Olá Mundo ${nome}`)');
+
+olaMundo("Cristiano")
