f2df9458232b15d24351ed2292dca066bd1e1419
Cristiano <contato@cristianoprogramador.com>
2020-12-27 09:37:47 -0300

    Conecta o Pino JS ao logflare

--- log/logger.js
+++ log/logger.js
@@ -1,3 +1,10 @@
+const { createWriteStream } = require("pino-logflare")
+
+const stream = createWriteStream({
+    apiKey: "API_KEY",
+    sourceToken: "SOURCE_TOKEN"
+});
+
 const pinoOptions = {
     name: "Exemplos",
     messageKey: "Description"
@@ -6,6 +13,8 @@ const pinoOptions = {
     //     levelFirst: true
     // }
 };
-const logger = require('pino')(pinoOptions, "./logs/info.log")
+
+// const logger = require('pino')(pinoOptions, "./logs/info.log") -> Gravar em arquivo
+const logger = require('pino')(pinoOptions, stream)
 
 module.exports = logger;
